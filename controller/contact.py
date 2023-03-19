53ef35d3f348729155d7ff3427cf80c33407a84c
DineshKumar BalaSubramanian <b.dineshkumar@live.com>
2023-03-19 18:50:07 +0530

    Contact feature in progress
    * Added Edit and Delete contact fucntions

--- controller/contact.py
+++ controller/contact.py
@@ -5,6 +5,14 @@ def create_contact():
     print 'Contact creation done'
 
 
-#Update contact
-def update_contact():
+#Edit contact
+def edit_contact():
     print 'Contact update done'
+
+#Get contact
+def get_contact():
+    print 'Get contact done'
+
+#Delete contact
+def delete_contact():
+    print 'Delete contact done'
