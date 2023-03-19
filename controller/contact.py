522d5601234f7eb9910aa7fdcb5b40327e358b88
DineshKumar BalaSubramanian <b.dineshkumar@live.com>
2023-03-19 18:42:38 +0530

    Contact feature initialized
    *Create functions for contact feature

---
+++ controller/contact.py
@@ -0,0 +1,10 @@
+#Contact controller implementation
+
+#Insert Contact
+def create_contact():
+    print 'Contact creation done'
+
+
+#Update contact
+def update_contact():
+    print 'Contact update done'
