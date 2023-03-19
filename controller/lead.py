a6dacc97a8666c982d0d7a9c8f67c783b944ac83
DineshKumar BalaSubramanian <b.dineshkumar@live.com>
2023-03-19 16:43:25 +0530

    Lead feature initialize
    *Create lead functionality done and tested.

---
+++ controller/lead.py
@@ -0,0 +1,6 @@
+#Lead controller implementation
+
+#Insert Lead
+def create_lead():
+    print 'Load creation done'
+
---
+++ model/lead.py
@@ -0,0 +1,6 @@
+#Lead model implementation
+
+#Insert Lead
+def insert_lead():
+    print 'Lead inserted into Database';
+
